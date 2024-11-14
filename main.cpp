6917ef7fbfc3b5a75f780efd4f7c604b33ad86f6
awry1 <92302998+awry1@users.noreply.github.com>
2024-11-14 15:56:21 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "Block.h"
 
-void split(std::string a, std::string b, std::string c, int &readCounter, int &writeCounter) {
+void split(std::string a, std::string b, std::string c, int &readCounter, int &writeCounter, int &seriesA, int &seriesB) {
     std::ofstream A(a, std::ios::binary);
     std::ofstream B(b, std::ios::binary);
     std::ifstream C(c, std::ios::binary);
     std::ofstream* outFile = &A;
     std::ifstream* inFile = &C;
+
+    int* series = &seriesA;
 
     Block input;
     Block output;
@@ -18,25 +20,34 @@ void split(std::string a, std::string b, std::string c, int &readCounter, int &w
         while (!input.empty()) {
             Record record = input.pop();
             if (record.Product < last) {
-                // Switch output file
-                std::cout << std::endl;
+                // switch output following fibonacci series
+                // TODO
+                (*series)++;
+                if (!output.empty()) {
+                    output.writeToFile((*outFile), writeCounter);
+                }
                 if (outFile == &A) {
                     outFile = &B;
+                    series = &seriesB;
+                    std::cout << std::endl;
                 }
                 else {
                     outFile = &A;
+                    series = &seriesA;
+                    std::cout << std::endl;
                 }
             }
             last = record.Product;
             std::cout << record.Product << ": ";
             record.print();
             if (output.full()) {
-                output.writeToFile((*outFile), writeCounter); /// <--- ERROR
+                output.writeToFile((*outFile), writeCounter);
             }
             output.records.push_back(record);
         }
         if (eof && !output.empty()) {
-            output.writeToFile((*outFile), writeCounter); /// <--- ERROR
+            (*series)++;
+            output.writeToFile((*outFile), writeCounter);
             input.clear();
         }
     }
@@ -48,14 +59,20 @@ void split(std::string a, std::string b, std::string c, int &readCounter, int &w
 void polyphaseMergeSort() {
     int readCounter = 0;
     int writeCounter = 0;
+    int seriesA = 0;
+    int seriesB = 0;
+    int seriesC = 0;
     std::string A = "A.bin";
     std::string B = "B.bin";
     std::string C = "C.bin";
 
-    split(A, B, C, readCounter, writeCounter);
+    split(A, B, C, readCounter, writeCounter, seriesA, seriesB);
+    // gaslight real number of series
 
     std::cout << std::endl << "Read counter: " << readCounter;
     std::cout << std::endl << "Write counter: " << writeCounter;
+    std::cout << std::endl << "Series A: " << seriesA;
+    std::cout << std::endl << "Series B: " << seriesB;
     
 
     //while (true) {
@@ -72,7 +89,7 @@ int main() {
 
     // Generate random records
     std::ofstream C("C.bin", std::ios::binary);
-    for (int i = 0; i < 24; i++) {
+    for (int i = 0; i < 5; i++) {
         Record record;
         record.randomize();
         record.print();
